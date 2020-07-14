d848e60f64467c17d1b07e89ff4ab2548c1c46c1
Arianaomi <arianaomi.lp@gmail.com>
2020-07-14 16:51:49 -0500

    :alembic: Read and write file with nodejs
    Read and write file with nodejs

--- fs-nodejs/fsModule.js
+++ fs-nodejs/fsModule.js
@@ -1,36 +1,33 @@
-
 /* Importando un  modulo/API 
     sintaxis: 
     const modulo/API = require('fs')
 
     -const porque no se cambia
 */
-const fs = require("fs");
+const fs = require('fs')
 
 /*  Function to read a file*/
 function readFile(cb) {
-  fs.readFile("./file.txt", "utf8", function (error, data) {
-    if (error) throw error;
-    cb(data);
-  });
+  fs.readFile('./file.txt', 'utf8', function (error, data) {
+    if (error) throw error
+    cb(data)
+  })
 }
 readFile(function (data) {
-  console.log(data);
-});
+  console.log(data)
+})
 
 /* Function to create a file */
 
 function writeFile(cb = () => {}) {
   fs.writeFile(
-    "./writeFile.txt",
-    "Creando un txt con node js 😆😎",
-    "utf8",
-    (error) => {
-      if (error) throw error;
-      cb("The file has been saved 😎");
+    './writeFile.txt',
+    'Creando un txt con node js 😆😎',
+    'utf8',
+    error => {
+      if (error) throw error
+      cb('The file has been saved 😎')
     }
-  );
+  )
 }
-writeFile((message) => console.log(message));
-
-
+writeFile(message => console.log(message))
